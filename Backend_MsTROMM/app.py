3cbd3546e524d16ff8b012e72f3a7ec8e0a4ed96
zinozzing <rlawlsgh1111@naver.com>
2021-11-18 15:09:26 +0900

    Add realtime date system

--- Backend_MsTROMM/app.py
+++ Backend_MsTROMM/app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify
 import urllib.request
 import urllib
 import json
+import datetime
 
 app = Flask(__name__)
 
@@ -36,32 +37,38 @@ def getNowCity(city) :
 
     # 받은 값 JSON 형태로 정제하여 반환
     items = json.loads(res)
-    print(items)
-    print("============================")
-    print("도시명 : %r" % items['name'])
-    print("============================")
-    print("날씨 : %r" % items['weather'][0]['main'])
-    print("날씨상세 : %r" % items['weather'][0]['description'])
-    print("아이콘 : %r  (사이트참조: https://openweathermap.org/weather-conditions)" % items['weather'][0]['icon'])
-    print("============================")
-    print("현재온도 : %r" % str(int(items['main']['temp'])-273.15))
-    print("체감온도 : %r" % str(int(items['main']['feels_like'])-273.15))
-    print("최저온도 : %r" % str(int(items['main']['temp_min'])-273.15))
-    print("최고온도 : %r" % str(int(items['main']['temp_max'])-273.15))
-    print("습도 : %r" % items['main']['humidity'])
-    print("기압 : %r" % items['main']['pressure'])
-    print("============================")
-    print("가시거리 : %r" % items['visibility'])
-    print("풍속 : %r" % items['wind']['speed'])
-    print("풍향 : %r" % items['wind']['deg'])
-    print("============================")
-    # print("강수량 : %r (시간당)" % items['rain']['1h']) # 비 올때만 생김 caution!!
-    print("============================")
-    print("구름 : %r " % items['clouds']['all'])
-    print("일출 : %r " % items['sys']['sunrise'])
-    print("일몰 : %r " % items['sys']['sunset'])
-    print("============================")
+    # print(items)
+    # print("============================")
+    # print("도시명 : %r" % items['name'])
+    # print("============================")
+    # print("날씨 : %r" % items['weather'][0]['main'])
+    # print("날씨상세 : %r" % items['weather'][0]['description'])
+    # print("아이콘 : %r  (사이트참조: https://openweathermap.org/weather-conditions)" % items['weather'][0]['icon'])
+    # print("============================")
+    # print("현재온도 : %r" % str(int(items['main']['temp'])-273.15))
+    # print("체감온도 : %r" % str(int(items['main']['feels_like'])-273.15))
+    # print("최저온도 : %r" % str(int(items['main']['temp_min'])-273.15))
+    # print("최고온도 : %r" % str(int(items['main']['temp_max'])-273.15))
+    # print("습도 : %r" % items['main']['humidity'])
+    # print("기압 : %r" % items['main']['pressure'])
+    # print("============================")
+    # print("가시거리 : %r" % items['visibility'])
+    # print("풍속 : %r" % items['wind']['speed'])
+    # print("풍향 : %r" % items['wind']['deg'])
+    # print("============================")
+    # # print("강수량 : %r (시간당)" % items['rain']['1h']) # 비 올때만 생김 caution!!
+    # print("============================")
+    # print("구름 : %r " % items['clouds']['all'])
+    # print("일출 : %r " % items['sys']['sunrise'])
+    # print("일몰 : %r " % items['sys']['sunset'])
+    # print("============================")
     return items
+
+@app.route('/date',methods = ['POST'])
+def Time():
+    now = datetime.datetime.now()
+    now_dict = {"a":now.strftime("%a"),"month":now.strftime("%b"),"day":now.strftime("%d")}
+    return jsonify(now_dict)
 
 
 if __name__ == "__main__":
