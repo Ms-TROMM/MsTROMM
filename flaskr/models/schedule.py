22e4e8edc8ca6cbbe8eac3d3efc36955f5546188
EJConnect <92378089+EJConnect@users.noreply.github.com>
2021-12-04 10:07:12 +0900

    Delete word2vec

--- flaskr/models/schedule.py
+++ flaskr/models/schedule.py
@@ -1,5 +1,5 @@
 from ..main import db
-
+from marshmallow import Schema, fields
 
 class Schedule(db.Model):
     __tablename__ = 'schedule'
@@ -8,9 +8,18 @@ class Schedule(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     title = db.Column(db.String(100))
     description = db.Column(db.String(255))
+    cont = db.Column(db.Integer)
     datetime = db.Column(db.DateTime, nullable=False, server_default=db.func.now())
 
+    def __init__(self,id, user_id, title, description, cont):
+        self.id = id
+        self.cont = cont
+        self.description = description
+        self.user_id = user_id
+        self.title = title
+        
     def create(self):
         db.session.add(self)
         db.session.commit()
         return self
+    
